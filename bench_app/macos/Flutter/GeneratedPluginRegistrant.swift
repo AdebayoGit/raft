860840eb26d504fa23bce92fe5288b55afe59979
TheEkundayo <ekundayo4hire@gmail.com>
2026-07-05 00:13:54 +0100

    chore: regenerate bench_app macos plugin registrant for raft_db

--- bench_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bench_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import isar_community_flutter_libs
 import objectbox_flutter_libs
 import path_provider_foundation
+import raft_db
 import realm
 import share_plus
 import sqflite_darwin
@@ -16,6 +17,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RaftDbFlutterPlugin.register(with: registry.registrar(forPlugin: "RaftDbFlutterPlugin"))
   RealmPlugin.register(with: registry.registrar(forPlugin: "RealmPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
